b9ff57d103264973bba7d053044bfaccc3d5ac8d
nhoutain <nicolas.houtain@student.uclouvain.be>
2015-07-16 22:45:45 +0200

    Ajout bar_mail

--- stat/stat.py
+++ stat/stat.py
@@ -115,6 +115,45 @@ def camenbert(i, sizes, labels, dpi, save) :
 
     #plt.show()
 
+def bars(i, sizes, colors, legendes, names, title, save): 
+
+    fig = figure(i,figsize=(8,8)
+    ax = fig.add_subplot(111, autoscale_on=False, xlim=(-0.65,7), ylim=(0,4))
+
+    N = len(sizes[0][0])
+
+    # the x locations for the groups
+    ind = np.arange(N)
+
+    width = 0.35
+    padding = ((len(sizes)-1)*width/2)
+
+    m=0
+    for (side, col, legende) in zip(sizes, colors, legendes) :
+        acc = map(lambda x: 0, sizes[0][0])
+        for (s,c, l) in zip(side, col, legende) :
+            plt.bar(ind+ m*width - padding , s, width, color=c, bottom=acc, label=l)
+            acc = map( lambda (x,y) : x+y, zip(acc, s))
+        m += 1
+
+
+    plt.ylabel('Pourcentage')
+    plt.title(title)
+
+    plt.xticks(ind+width/2., names, rotation='vertical' )
+    plt.margins(0.3)
+    # Tweak spacing to prevent clipping of tick-labels
+    plt.subplots_adjust(bottom=0.25)
+
+    plt.yticks(np.arange(0,101,10))
+
+    plt.legend(loc=2)
+
+    savefig(save)
+
+
+
+
 
 #  ____  _        _   
 # / ___|| |_ __ _| |_ 
@@ -134,18 +173,45 @@ base.insert(0,(papers, "Global"))
 
 # Generated piechart 
 i=0
+#for (ps, n) in base :
+#    nSend     = count(lambda p: SEND in p.infos[MAIL], ps)
+#    nResponse = count(lambda p: RESPONSE in p.infos[MAIL], ps)
+#    nComplet  = count(lambda p: COMPLET in p.infos[PINFO], ps)
+#    nSelf     = count(lambda p: SELF in p.infos[PINFO], ps)
+#    nHardware = count(lambda p: HARD in p.infos[PINFO], ps)
+#    nMail     = len(ps)
+#
+#    camenbert(i,[nSend, nResponse, nComplet, nSelf, nHardware],
+#                ['No reply', 'Response', 'Complet', 'Self', 'Hardware'] , 1000, "piechart_mail/piechart_mail-{}.png".format(n))
+#    i+=1
+
+
+nSend     = []
+nResponse = []
+nComplet  = []
+nSelf     = []
+nHardware = []
+nMail     = []
+names     = []
+
 for (ps, n) in base :
-    nSend = count(lambda p: SEND in p.infos[MAIL], ps)
-    nResponse = count(lambda p: RESPONSE in p.infos[MAIL], ps)
-    nComplet = count(lambda p: COMPLET in p.infos[PINFO], ps)
-    nSelf = count(lambda p: SELF in p.infos[PINFO], ps)
-    nHardware = count(lambda p: HARD in p.infos[PINFO], ps)
     nMail = len(ps)
+    nSend    .append( (100.0/nMail)*count(lambda p: SEND in p.infos[MAIL], ps) )
+    nResponse.append( (100.0/nMail)*count(lambda p: RESPONSE in p.infos[MAIL], ps) )
+    nComplet .append( (100.0/nMail)*count(lambda p: COMPLET in p.infos[PINFO], ps) )
+    nSelf    .append( (100.0/nMail)*count(lambda p: SELF in p.infos[PINFO], ps) )
+    nHardware.append( (100.0/nMail)*count(lambda p: HARD in p.infos[PINFO], ps) )
+    names    .append(n)
 
-    camenbert(i,[nSend, nResponse, nComplet, nSelf, nHardware],
-                ['No reply', 'Response', 'Complet', 'Self', 'Hardware'] , 1000, "piechart_mail/piechart_mail-{}.png".format(n))
-    i+=1
 
+bars(i,[[nComplet, nSelf, nHardware], [nSend, nResponse]],
+            [['r', 'g', 'b'], ['y', 'r']],
+            [['Complet', 'Self contains', 'Hardware'], ['Wait response', 'Response']],
+            names, "Result of Analysis.csv", 'bar_mail/bar_mail-all.png')
+                #['No reply', 'Response', 'Complet', 'Self', 'Hardware'] , 1000, "piechart_mail/piechart_mail-{}.png".format(n))
 
+i+=1
+
+#bars(1, [[20, 35, 30, 35, 27], [25, 32, 34, 20, 25], [31, 37, 2, 26, 20]], ['y', 'r', 'g'])
 
 
